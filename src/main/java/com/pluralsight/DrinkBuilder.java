9f40927b7984cb4fa16419838aed7317693e8b07
Student <enesyilmaz4525@gmail.com>
2024-11-12 15:55:57 -0500

    buildDrink method added to the DrinkBuilder class. and topping class conventor as a abstrack class.

--- src/main/java/com/pluralsight/DrinkBuilder.java
+++ src/main/java/com/pluralsight/DrinkBuilder.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
+import java.util.Scanner;
 
 public class DrinkBuilder {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public Drink buildDrink() {
+        System.out.println("\n--- Drink Maker ---");
+        System.out.print("Size (Small/Medium/Large): ");
+        String size = scanner.nextLine();
+        System.out.print("Flavor (Cola/Lemonade/Orange/Water): ");
+        String flavor = scanner.nextLine();
+        return new Drink(size, flavor);
+    }
+
 }
