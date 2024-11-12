2a4e46504362a1d15b896602b8c99003eb1020e3
Student <enesyilmaz4525@gmail.com>
2024-11-12 15:31:14 -0500

    Price set based on size in the Drink class constructor.

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -8,7 +8,18 @@ public class Drink {
     public Drink(String size, String flavor) {
         this.size = size;
         this.flavor = flavor;
+
+        if (size.equalsIgnoreCase("small")) {
+            this.price = 2.00;
+        } else if (size.equalsIgnoreCase("medium")) {
+            this.price = 2.50;
+        } else if (size.equalsIgnoreCase("large")) {
+            this.price = 3.00;
+        }
+
     }
+
+
 
 
 
