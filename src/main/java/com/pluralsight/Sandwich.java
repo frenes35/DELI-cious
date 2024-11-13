feefba4689fb586ebd55e822a18d57c69a6ef0e5
Student <enesyilmaz4525@gmail.com>
2024-11-13 09:39:30 -0500

    Attributes added to the Sandwich class.

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
+import java.util.List;
+
 public class Sandwich {
+    private String size;
+    private String breadType;
+    private boolean toasted;
+    private List<Topping> toppings;
+    private List<Sauce> sauces;
 }
