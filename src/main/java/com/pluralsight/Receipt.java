d4d7156e10052355a916f912e062ac589d1da1c8
Student <enesyilmaz4525@gmail.com>
2024-11-13 11:18:06 -0500

    has been completed Receipt class.

--- src/main/java/com/pluralsight/Receipt.java
+++ src/main/java/com/pluralsight/Receipt.java
@@ -1,5 +1,26 @@
 package com.pluralsight;
 
-public class Receipt {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+public class Receipt {
+    public static void saveOrder(Order order) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String timestamp = LocalDateTime.now().format(formatter);
+        String fileName = "receipts/" + timestamp + ".txt";
+        java.io.File directory = new java.io.File("receipts");
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+        try (FileWriter writer = new FileWriter(fileName)) {
+            System.out.println("Order Summary:\n------------------- ");
+            writer.write(order.toString());
+            writer.write("\nTotal Cost: $" + order.calculateTotal() + "\n");
+            System.out.println("Receipt saved as " + fileName);
+        } catch (IOException e) {
+            System.err.println("Error saving receipt: " + e.getMessage());
+        }
+    }
 }
