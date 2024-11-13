1f0941e36d7b21cd53303adfb1fef0bae8f2e30e
Student <enesyilmaz4525@gmail.com>
2024-11-13 08:44:04 -0500

    Constructor added to the Topping class.

--- src/main/java/com/pluralsight/PremiumTopping.java
+++ src/main/java/com/pluralsight/PremiumTopping.java
@@ -1,4 +1,4 @@
 package com.pluralsight;
 
-public class PremiumTopping ex{
+public class PremiumTopping {
 }
--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -1,4 +1,26 @@
 package com.pluralsight;
 
 public abstract class Topping {
+
+    private String name;
+    private double price;
+
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " ($" + price + ")";
+    }
 }
+
