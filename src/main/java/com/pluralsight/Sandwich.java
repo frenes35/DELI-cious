14f36d37394a517a34a1be58c80325549e483082
Student <enesyilmaz4525@gmail.com>
2024-11-13 09:42:04 -0500

    Added constructor to Sandwich class.

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -8,4 +8,12 @@ public class Sandwich {
     private boolean toasted;
     private List<Topping> toppings;
     private List<Sauce> sauces;
+
+    public Sandwich(String size, String breadType, boolean toasted, List<Topping> toppings, List<Sauce> sauces) {
+        this.size = size;
+        this.breadType = breadType;
+        this.toasted = toasted;
+        this.toppings = toppings;
+        this.sauces = sauces;
+    }
 }
