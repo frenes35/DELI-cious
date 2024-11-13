9188be888604d0287b5221b531c31a542fc6c480
Student <enesyilmaz4525@gmail.com>
2024-11-13 09:28:55 -0500

    Sauce class hass been completed.

--- src/main/java/com/pluralsight/Sauce.java
+++ src/main/java/com/pluralsight/Sauce.java
@@ -2,5 +2,20 @@ package com.pluralsight;
 
 public class Sauce {
 
+    private String name;
+
+    public Sauce(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " sauce";
+    }
+
 
 }
