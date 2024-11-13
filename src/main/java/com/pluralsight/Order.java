d092aa054c2d5db9f222f9e06fe59211691191d9
Student <enesyilmaz4525@gmail.com>
2024-11-13 10:28:07 -0500

    has been added calculateTotal method to Order class.

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,44 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Order {
+    private List<Sandwich> sandwiches;
+    private List<Drink> drinks;
+    private List<Chips> chips;
+
+    public Order() {
+        sandwiches = new ArrayList<>();
+        drinks = new ArrayList<>();
+        chips = new ArrayList<>();
+    }
+
+    public void addSandwich(Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
+
+    public void addDrink(Drink drink) {
+        drinks.add(drink);
+    }
+
+    public void addChips(Chips chips) {
+        this.chips.add(chips);
+    }
+
+    public double calculateTotal() {
+        double sandwichTotal = 0;
+        for (Sandwich sandwich : sandwiches) {
+            sandwichTotal += sandwich.calculatePrice();
+        }
+        double drinkTotal = 0;
+        for (Drink drink : drinks) {
+            drinkTotal += drink.getPrice();
+        }
+        double chipsTotal = chips.size() * 1.50;
+        return sandwichTotal + drinkTotal + chipsTotal;
+    }
+
+
 }
