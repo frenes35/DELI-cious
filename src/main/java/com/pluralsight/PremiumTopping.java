377070279fc8ca1012541bd5adb631b44badd157
Student <enesyilmaz4525@gmail.com>
2024-11-13 09:10:38 -0500

    PremiumTopping class has been completed.

--- src/main/java/com/pluralsight/PremiumTopping.java
+++ src/main/java/com/pluralsight/PremiumTopping.java
@@ -1,4 +1,8 @@
 package com.pluralsight;
 
-public class PremiumTopping {
+public class PremiumTopping extends Topping {
+    public PremiumTopping(String name, double price) {
+        super(name, price);
+    }
+
 }
