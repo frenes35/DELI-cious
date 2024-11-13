3c9a4dee825be31928a95adc3f3e1f4a14df7942
Student <enesyilmaz4525@gmail.com>
2024-11-13 11:21:57 -0500

    has been started DELI-cious Order System menu in OrderSystem class.

--- src/main/java/com/pluralsight/OrderSystem.java
+++ src/main/java/com/pluralsight/OrderSystem.java
@@ -3,5 +3,14 @@ package com.pluralsight;
 public class OrderSystem {
     public static void main(String[] args) {
 
+        boolean running = true;
+        while (running) {
+            System.out.println("---- DELI-cious Order System ----");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.print("Your choice: ");
+
+        }
+
     }
 }
